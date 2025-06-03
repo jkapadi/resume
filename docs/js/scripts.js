013c756f3345f4646cac8306fdd43ed5ec92a3e3
jkapadi <jkapadi206@gmail.com>
2025-06-03 17:58:26 +0530

    update

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2025 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+* Start Bootstrap - Resume v7.0.6 (https://github.com/resume)
+* Copyright 2013-2025 Jkapadi
+* Licensed under MIT (https://github.com/StartBootstrap/resume/blob/master/LICENSE)
 */
 //
 // Scripts
